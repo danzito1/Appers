6c32531c002abda10593a7405d1affdab6eedd41
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-03-01 14:03:57 -0300

    Update index.js

--- devs/hire/js/index.js
+++ devs/hire/js/index.js
@@ -1,1 +1,10 @@
-
+window.onload = function() {
+    var Lang = navigator.language;
+    Lang = Lang.split("-")[0];
+    
+    if (Lang === "pt") {
+        document.title = "Contrate-nos";
+    } else if (Lang === "es") {
+        document.title = "Contratanos";
+    }
+}
